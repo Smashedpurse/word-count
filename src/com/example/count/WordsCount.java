e377740b25ce45c301f96a073f11bdfd2c858dad
Ivan Rubio <smashedpurse@gmail.com>
2022-12-25 10:48:20 -0600

    implement the sintaxis to reed the file txt

--- src/com/example/count/WordsCount.java
+++ src/com/example/count/WordsCount.java
@@ -1,5 +1,30 @@
 package com.example.count;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class WordsCount {
-    public String readFile ()
+
+    public String readFile (File file){
+
+        String lines = "";
+
+        try{
+            Scanner sc = new Scanner(file);
+
+            while(sc.hasNextLine()){
+                lines += sc.hasNextLine();
+                System.out.println(lines);
+            }
+
+        }catch (FileNotFoundException e){
+            System.out.println( "Exepction with the file" + e );
+        }
+
+
+        return null;
+
+    }
+
 }
